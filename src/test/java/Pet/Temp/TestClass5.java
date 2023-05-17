a2724b1b9c69aa9befe74e73863160d682d64b29
fawwazfahad <fawwazfahad2@gmail.com>
2023-05-17 20:31:35 +0530

    Combined multiple commits

--- src/test/java/Pet/Temp/TestClass5.java
+++ src/test/java/Pet/Temp/TestClass5.java
@@ -5,6 +5,9 @@ public class TestClass5 {
 	public static void main(String[] args)
 	{
 	System.out.println("Test1");
+	System.out.println("Test2");
+	System.out.println("Test3");
+	System.out.println("Test4");
 	}
 
 }
